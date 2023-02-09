881eded495c48cd4e53ffe50c2cb0fa9f69b83bf
ogunsolu qudus <ogunsolu30@gmail.com>
2023-02-09 16:58:39 +0000

    created a reponsive navbar

--- ShadeOn/wwwroot/js/site.js
+++ ShadeOn/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+var searchIcon = document.querySelector('.fa-search');
+var closeIcon = document.querySelector('.fa-times');
+var search = document.getElementById('search');
+
+searchIcon.onclick = () => {
+	search.classList.add('expand');
+}
+closeIcon.onclick = () => {
+	search.classList.remove('expand');
+}
+
+var button = document.querySelector('.navbar-button');
+var menu = document.querySelector('.nav-menu');
+button.onclick = () => {
+	menu.classList.toggle('expand-mobile');
+	button.classList.toggle('expand-icon');
+
+}
