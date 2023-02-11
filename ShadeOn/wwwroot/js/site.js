938dd81bb998a1522f196785a3f3b5fcf3e53778
ogunsolu qudus <ogunsolu30@gmail.com>
2023-02-11 16:11:52 +0000

    Navbar and hero section

--- ShadeOn/wwwroot/js/site.js
+++ ShadeOn/wwwroot/js/site.js
@@ -3,22 +3,5 @@
 
 // Write your JavaScript code.
 
+ // Slider code
 
-var searchIcon = document.querySelector('.fa-search');
-var closeIcon = document.querySelector('.fa-times');
-var search = document.getElementById('search');
-
-searchIcon.onclick = () => {
-	search.classList.add('expand');
-}
-closeIcon.onclick = () => {
-	search.classList.remove('expand');
-}
-
-var button = document.querySelector('.navbar-button');
-var menu = document.querySelector('.nav-menu');
-button.onclick = () => {
-	menu.classList.toggle('expand-mobile');
-	button.classList.toggle('expand-icon');
-
-}
