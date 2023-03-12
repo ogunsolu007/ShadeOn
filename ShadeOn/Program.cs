1b350390974471520cfaf9c319d811b69fa95b21
ogunsolu qudus <ogunsolu30@gmail.com>
2023-03-12 09:53:47 +0000

    facebook login added

--- ShadeOn/Program.cs
+++ ShadeOn/Program.cs
@@ -14,12 +14,25 @@ UseSqlServer(builder.Configuration.GetConnectionString("Default")));
 
 //builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppDataContext>();
 
+//builder.Services.AddDefaultIdentity<AppUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AppDataContext>();
+
 
 builder.Services.AddIdentity<AppUser, IdentityRole>()
 .AddDefaultTokenProviders()
  .AddDefaultUI()
  .AddEntityFrameworkStores<AppDataContext>();
 
+builder.Services.AddAuthentication().AddMicrosoftAccount(microsoftOptions =>
+{
+    microsoftOptions.ClientId = builder.Configuration["Authentication:Microsoft:ClientId"];
+    microsoftOptions.ClientSecret = builder.Configuration["Authentication:Microsoft:ClientSecret"];
+});
+
+builder.Services.AddAuthentication().AddFacebook(options =>
+{
+    options.AppId = "1336744393848112";
+    options.AppSecret = "e15d79c3dbb5142f958387844f1dfb95";
+});
 
 builder.Services.AddAuthorization();
 
